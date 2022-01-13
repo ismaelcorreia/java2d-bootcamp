bf445aa057897a619e3cabfbdd1da898b9918fa8
Ismael Clever <58177407+ismaelcorreia@users.noreply.github.com>
2022-01-13 17:37:20 +0100

    Criando objetos sucessivos

--- src/controllers/Canva.java
+++ src/controllers/Canva.java
@@ -1,21 +1,47 @@
 package controllers;
 
 import java.awt.*;
-import java.awt.geom.AffineTransform;
 import java.awt.geom.Ellipse2D;
-import java.awt.geom.Rectangle2D;
-
+import java.awt.geom.GeneralPath;
+/** Para comentar façam ctrl + / */
 public class Canva extends Component {
 
     @Override
     public void paint(Graphics g) {
+
         Graphics2D g2 = (Graphics2D) g;
-//        g2.scale(1.5, 1.5);
-        g2.setPaint(new Color(11, 77, 164));
+
+        g2.setPaint(new Color(34, 105, 210, 137));
         desenharGrelha(g2);
-            for (int i = 0; i < 5; i++) {
-                desenharCirculo(g2, 150 + ((100 + 10) * i) ,100, 100, 100, new Color(199, 97, 227));
-            }
+
+
+
+        GeneralPath lapis = new GeneralPath(); // O nosso lápis
+        Point coordenadas = new Point(500, 100); // coordenadas do lápis
+
+        g2.setPaint(new Color(32, 56, 100)); // cor do lápis
+
+        lapis.moveTo(coordenadas.x, coordenadas.y); // movendo o lápis para a coordenadas
+        lapis.lineTo(coordenadas.x -= 100, coordenadas.y += 100); // Atualizando x - 100px e + 100px para y
+        lapis.lineTo(coordenadas.x += 200, coordenadas.y); // Atualizando + 200px em x e mantendo y
+        lapis.closePath(); // Unindo as extremidades
+
+        g2.fill(lapis); // representado o desenho de forma preenchida
+
+
+//        lapis.moveTo(coordenadas.x, coordenadas.y); // movendo o lápis para a coordenadas
+//        lapis.lineTo(coordenadas.x - 100, coordenadas.y + 100); // Desenhando - 100px em x e + 100px para y
+//        lapis.lineTo(coordenadas.x + 100, coordenadas.y + 100); // Desenhando + 100px em x e + 100px para y
+//        lapis.closePath();  // Unir este ponto ao ponto incial
+//
+//        g2.fill(lapis); // representado o desenho de forma preenchida
+
+//        lapis.closePath();
+
+//        g2.scale(1.5, 1.5);
+//            for (int i = 0; i < 5; i++) {
+//                desenharCirculo(g2, 150 + ((100 + 10) * i) ,100, 100, 100, new Color(199, 97, 227));
+//            }
 
 
 //        for (int espaco = 0, i = 0; i < (100+espaco) * 5; i += (100+espaco)) {
@@ -27,15 +53,17 @@ public class Canva extends Component {
         Ellipse2D ellipse2D = new Ellipse2D.Double(x,  y,  w,  h);
         g.setPaint(color);
         g.fill(ellipse2D);
+        g.fillRect(10, 10, 120, 100);
+        g.fillOval(10, 10, 120, 120);
         return ellipse2D;
     }
 
-    private Rectangle2D desenharRectangulo(Graphics2D g, int x, int y, int w, int h, Color color) {
-        Rectangle2D rectangle = new Rectangle2D.Double(x,  y,  w,  h);
-        g.setPaint(color);
-        g.fill(rectangle);
-        return rectangle;
-    }
+//    private Rectangle2D desenharRectangulo(Graphics2D g, int x, int y, int w, int h, Color color) {
+//        Rectangle2D rectangle = new Rectangle2D.Double(x,  y,  w,  h);
+//        g.setPaint(color);
+//        g.fill(rectangle);
+//        return rectangle;
+//    }
 
     private void desenharGrelha(Graphics2D g) {
         for (int i = 0; i < getWidth(); i+= 10) {
